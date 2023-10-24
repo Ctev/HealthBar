62a412047161f5f6c04e4fce17e2be6424508ba2
Ctev <casmosh@mail.ru>
2023-10-24 04:02:59 +0300

    seventh commit

--- Health Bar/Assets/Scripts/Health.cs
+++ Health Bar/Assets/Scripts/Health.cs
@@ -1,26 +1,60 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
+    // Health самосто€тельна€ сущность, никак не завис€ща€ от слайдера/бара.
+    // ћетоды Heal и Damage не должны ничего возвращать. ѕри их вызове происходит
+    // изменение здоровь€, проверка на мин макс и вызов ивента, сообщающего всем
+    // об изменении здоровь€
+
     [SerializeField] private Slider _slider;
+    [SerializeField] private Button _healButton;
+    [SerializeField] private Button _damageButton;
 
     private float _currentHealth;
     private float _maxHealth;
+    private float _duration;
+    private float _receivedHealth;
+    private float _lostHealth;
+
+    private void OnValidate()
+    {
+        _maxHealth = _slider.maxValue;
+        _duration = 1.2f;
+        _receivedHealth = 10f;
+        _lostHealth = 10f;
+    }
+
+    private void Start()
+    {
+        _healButton.onClick.AddListener(Heal);
+        _damageButton.onClick.AddListener(Damage);
+    }
 
     private void Update()
     {
-        _currentHealth = _slider.value;
-        _maxHealth = _slider.maxValue;
+        _slider.value = _currentHealth;
     }
 
-    public float Heal(float receivedHealth)
+    private void Heal()
     {
-        return _currentHealth += receivedHealth;
+        //if (_currentHealth + receivedHealth > _maxHealth)
+        //    _currentHealth = _maxHealth;
+        //else
+        //    _currentHealth += receivedHealth;
+
+        _slider.DOValue(_currentHealth += _receivedHealth, _duration);
+        //_currentHealth += _receivedHealth;
+
+        //_slider.DOValue(_currentHealth, _duration);
     }
 
-    public float Damage(float lostHealth)
+    private void Damage()
     {
-        return _currentHealth -= lostHealth;
+        _slider.DOValue(_currentHealth -= _lostHealth, _duration);
+        //_currentHealth -= _lostHealth;
     }
 }
--- Health Bar/Assets/Scripts/HealthBar.cs
+++ Health Bar/Assets/Scripts/HealthBar.cs
@@ -1,36 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
 
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
-    [SerializeField] private Health _health;
+    //[SerializeField] private Health _health;
     [SerializeField] private Text _healthPointsLabel;
 
-    private float _duration;
-    private float _receivedHealth;
-    private float _lostHealth;
+    //private float _duration;
+    //private float _receivedHealth;
+    //private float _lostHealth;
 
-    private void OnValidate()
-    {
-        _duration = 1.2f;
-        _receivedHealth = 10f;
-        _lostHealth = 10f;
-    }
+    //private void OnValidate()
+    //{
+    //    _duration = 1.2f;
+    //    _receivedHealth = 10f;
+    //    _lostHealth = 10f;
+    //}
 
     private void Update()
     {
-        _healthPointsLabel.text = _slider.value + " - " + _slider.maxValue;
-    }
-
-    public void Add()
-    {
-        _slider.DOValue(_health.Heal(_receivedHealth), _duration);
-    }
-
-    public void Decrease()
-    {
-        _slider.DOValue(_health.Damage(_lostHealth), _duration);
+        _healthPointsLabel.text = _slider.value + " / " + _slider.maxValue;
     }
 }
